0cced2c4485f85fd3f761939b313c9fbdfe3bc92
Edward Schults <edward.schults@gmail.com>
2017-09-20 12:31:04 +0200

    cities reducer

---
+++ src/reducers/cities_reducer.js
@@ -0,0 +1,28 @@
+export default function() {
+  return [
+    { name: 'Chengdu', address: '399 Fucheng Avenue West Section, Chengdu, Sichuan, China' },
+    { name: 'Florianópolis', address: 'Rod. José Carlos Daux, 4150 - Saco Grande, Florianópolis - SC, 88032-005' },
+    { name: 'Tokyo', address: '2-11-3 Meguro, Meguro-ku, Tokyo ' },
+    { name: 'Brussels', address: 'Place Sainte-Gudule 5, 1000 Brussels' },
+    { name: 'Amsterdam', address: 'Overhoeksplein 2, 1031 KS Amsterdam' },
+    { name: 'Bali', address: 'Jalan Raya Batu Bolong, Canggu, Badung Regency, Bali, Indonesia' },
+    { name: 'Budapest', address: 'Budapest, Hungary' },
+    { name: 'Milan', address: 'Milan, Metropolitan City of Milan, Italy' },
+    { name: 'Montréal', address: '5333, avenue Casgrain, 102, Montréal (Québec), H2T1X3' },
+    { name: 'Shanghai', address: '129 West Yan\'an Road, JingAn District, Shanghai' },
+    { name: 'Rio de Janeiro', address: 'Rua Duque Estrada, 41 - Gávea, Rio de Janeiro' },
+    { name: 'Belo Hoizonte', address: 'Av. Afonso Pena, 2881, Belo Horizonte' },
+    { name: 'São Palo', address: 'Rua Mourato Coelho, 1404 – Vila Madalena, São Paulo - SP' },
+    { name: 'Barcelona', address: 'Travessera de Dalt, 33, 08024 Barcelona' },
+    { name: 'London', address: '14-22 Elder St, London E1 6BT' },
+    { name: 'Copenhagen', address: 'Ahornsgade 15, 2200 KBH N' },
+    { name: 'Nantes', address: '11 impasse Juton, Nantes' },
+    { name: 'Lisbon', address: 'Avenida Casal Ribeiro 28, Lisbon' },
+    { name: 'Aix - Marseille', address: 'Rue Joseph Biaggi, 13003 Marseille' },
+    { name: 'Bordeaux', address: '107 cours Balguerie Stuttenberg, 33300' },
+    { name: 'Lille', address: '2 Avenue des Saules, 59000 Lille' },
+    { name: 'Paris', address: '16 Villa Gaudelet, 75011 Paris' },
+    { name: 'Lyon', address: '23 Rue Paul Montrochet, 69002 Lyon' },
+    { name: 'Berlin', address: 'Rudi-Dutschke-Straße 26, 10969 Berlin' }
+  ]
+}
